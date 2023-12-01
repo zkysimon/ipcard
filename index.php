dd32c15e80083bc173f4d390a70197e209a6d20c
zkysimon <zkygo@outlook.com>
2023-12-01 12:24:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -259,7 +259,7 @@ class App
     public function getAddress()
     {
         //获取IP及城市代码
-        $url = "http://restapi.amap.com/v3/ip?key=" . $this->key;
+        $url = "http://restapi.amap.com/v3/ip?key=" . $this->key . "&ip=" . $this->ip;
         $UserAgent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 3.0.04506; .NET CLR 3.5.21022; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
